2ea9e24582aa3ebc34a720d057c84b77ed9d45c1
Cori Darbonne <CoriLDarbonne@gmail.com>
2021-02-01 14:33:01 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package typesVariablesBranchingHomework;
+
+public class Main {
+
+	public static void main(String[] args) {
+		class Student {
+			String studentType = "OnlineStudent";
+			String studentTeacher = "Patterson";
+			String studentProject = "Project";
+		
+			System.out.println(studentType)();
+			
+	}
+
+		byte userAge = 20;
+		short numberOfStudents = 45;
+		int numberOfEmployees = 500;
+		long numberofTexasResidents: 1149586859859505L
+		float hourlyRate = 70.5;
+		double numberOfHours = 6384.5;
+		char grade = 'A';
+		byte levels = 2, userExperience = 5;
+		boolean use = true;
+		int x = 87;
+		
+		System.out.print(use);
+		System.out.print(char);
+		
+}
+	
+	public static class School {
+	    public static int capacity = 12;
+	    private boolean full = false;
+	
+	    System.out.println(capacity);
+	}
+	}
